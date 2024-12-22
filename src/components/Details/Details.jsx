1caafaffed9f68fd55e4772152020de50226c960
Yuliia_Harashchenko <yharashchenko@ukr.net>
2024-12-22 15:20:40 +0200

    add features

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -5,31 +5,27 @@ import { useRef } from "react";
 import css from "./Details.module.css";
 // import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import { FaStar } from "react-icons/fa";
 import clsx from "clsx";
 
 // import { NavLink } from "react-router-dom";
 
 const Details = () => {
   const location = useLocation();
-  const backLink = useRef(location?.state ?? "/");
+  const backLink = useRef(location?.state ?? "/catalog");
   const details = useSelector(selectDetails);
-  console.log(details);
+  // console.log(details);
 
   const camperLoading = useSelector((state) => state.campers.camperLoading);
   const camperError = useSelector((state) => state.campers.camperError);
 
-  // Стан завантаження
   if (camperLoading) {
-    return <div>Завантаження...</div>; // Можна додати спінер чи стилізувати
+    return <div>Завантаження...</div>;
   }
 
-  // Стан помилки
   if (camperError) {
     return <div>Помилка: {camperError}</div>;
   }
 
-  // Якщо дані відсутні
   if (!details) {
     return <div>Деталі кемпера недоступні</div>;
   }
@@ -38,60 +34,68 @@ const Details = () => {
     "https://dummyimage.com/400x600/cdcdcd/000.jpg&text=No+poster";
 
   return (
-    <div>
+    <>
       <Link className={css.backLink} to={backLink.current}>
         ← Go back
       </Link>
-      <h3 className={css.detailsTitle}>{details.name}</h3>
-      <div className={css.ratingLocContainer}>
-        <div className={css.ratingContainer}>
-          <FaStar className={css.starIcon} />
-          <p className={css.reviews}>
-            {details.rating}({details.reviews ? details.reviews.length : 0}
-            Reviews)
-          </p>
+      <div className={css.container}>
+        <h3 className={css.detailsTitle}>{details.name}</h3>
+        <div className={css.textContainer}>
+          <div className={css.ratingContainer}>
+            <svg width="16" height="15" className={css.starIcon}>
+              <use href="/sprite.svg#star" />
+            </svg>
+            <p className={css.reviews}>
+              {details.rating}({details.reviews ? details.reviews.length : 0}{" "}
+              Reviews)
+            </p>
+          </div>
+          <div className={css.locationContainer}>
+            <svg width="16" height="16">
+              <use href="/sprite.svg#map" />
+            </svg>
+            <p className={css.location}>{details.location}</p>
+          </div>
         </div>
-        <div className={css.locationContainer}>
-          <svg width="16" height="16">
-            <use href="/sprite.svg#map" />
-          </svg>
-          <p className={css.location}>{details.location}</p>
+        <p className={css.detailsPrice}>&#x20AC;{details.price}</p>
+        {/* <div className={css.imgContainer}> */}
+        {details.gallery.length > 0 ? (
+          <ul className={css.imgContainer}>
+            {details.gallery.map((item, index) => (
+              <li key={index} className={css.detailsItem}>
+                <img
+                  src={item.thumb}
+                  alt={`Gallery image ${index + 1}`}
+                  className={css.detailsImage}
+                />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          defaultImg
+        )}
+        {/* </div> */}
+        <p className={css.text}>{details.description}</p>
+
+        <nav className={css.nav}>
+          <NavLink
+            className={({ isActive }) => clsx(css.link, isActive && css.active)}
+            to="features"
+          >
+            Features
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => clsx(css.link, isActive && css.active)}
+            to="reviews"
+          >
+            Reviwes
+          </NavLink>
+        </nav>
+        <div className={css.lowContainer}>
+          <Outlet />
         </div>
       </div>
-      <p className={css.detailsPrice}>&#x20AC;{details.price}</p>
-      {/* <div className={css.imgContainer}> */}
-      {details.gallery.length > 0 ? (
-        <ul className={css.imgContainer}>
-          {details.gallery.map((item, index) => (
-            <li key={index} className={css.detailsItem}>
-              <img
-                src={item.thumb}
-                alt={`Gallery image ${index + 1}`}
-                className={css.detailsImage}
-              />
-            </li>
-          ))}
-        </ul>
-      ) : (
-        defaultImg
-      )}
-      {/* </div> */}
-      <p className={css.text}>{details.description}</p>
-      <nav className={css.nav}>
-        <NavLink
-          className={({ isActive }) => clsx(css.link, isActive && css.active)}
-          to="features"
-        >
-          Cast
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => clsx(css.link, isActive && css.active)}
-          to="reviews"
-        >
-          Reviwes
-        </NavLink>
-      </nav>
-    </div>
+    </>
   );
 };
 
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,5 +1,63 @@
+import { useSelector } from "react-redux";
+import css from "./Features.module.css";
+import { selectDetails } from "../../redux/campers/selectors.js";
+import { BsCupHot, BsFuelPump } from "react-icons/bs";
+
 const Features = () => {
-  return <div>Features</div>;
+  const details = useSelector(selectDetails);
+
+  return (
+    <div className={css.featuresContainer}>
+      <ul className={css.categoriesList}>
+        {details.transmission === "automatic" && (
+          <li className={css.categoriesListItem}>
+            <svg width="20" height="16">
+              <use href="/sprite.svg#automatic" />
+            </svg>
+            Automatic
+          </li>
+        )}
+        {details.AC && (
+          <li className={css.categoriesListItem}>
+            <svg width="20" height="20">
+              <use href="/sprite.svg#automatic" />
+            </svg>
+            AC
+          </li>
+        )}
+        <li className={css.categoriesListItem}>
+          <BsFuelPump className={css.icon} />
+          {details.engine === "patrol" ? "Patrol" : "Diesel"}
+        </li>
+        {details.kitchen && (
+          <li className={css.categoriesListItem}>
+            <BsCupHot className={css.icon} />
+            Kitchen
+          </li>
+        )}
+      </ul>
+
+      <p className={css.vehicle}>Vehicle details</p>
+      <div className={css.lowContainer}>
+        <ul>
+          <li>Form</li>
+          <li>Length</li>
+          <li>Width</li>
+          <li>Height</li>
+          <li>Tank</li>
+          <li>Consumption</li>
+        </ul>
+        <ul>
+          <li>{details.form}</li>
+          <li>{details.length}</li>
+          <li>{details.width}</li>
+          <li>{details.height}</li>
+          <li>{details.tank}</li>
+          <li>{details.consumption} </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Features;
