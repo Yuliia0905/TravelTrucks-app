417a18b76ec6c12cd232159733aef6395137f1a5
Yuliia_Harashchenko <yharashchenko@ukr.net>
2024-12-22 13:40:39 +0200

    detailsPage basic structure

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,38 +1,67 @@
 import { useSelector } from "react-redux";
 import { selectDetails } from "../../redux/campers/selectors.js";
-// import css from "./Details.module.css";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { useRef } from "react";
+import css from "./Details.module.css";
 // import { Suspense } from "react";
-// import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
+import clsx from "clsx";
 
 // import { NavLink } from "react-router-dom";
 
 const Details = () => {
+  const location = useLocation();
+  const backLink = useRef(location?.state ?? "/");
   const details = useSelector(selectDetails);
-  //   console.log(details);
+  console.log(details);
+
+  const camperLoading = useSelector((state) => state.campers.camperLoading);
+  const camperError = useSelector((state) => state.campers.camperError);
+
+  // Стан завантаження
+  if (camperLoading) {
+    return <div>Завантаження...</div>; // Можна додати спінер чи стилізувати
+  }
+
+  // Стан помилки
+  if (camperError) {
+    return <div>Помилка: {camperError}</div>;
+  }
+
+  // Якщо дані відсутні
+  if (!details) {
+    return <div>Деталі кемпера недоступні</div>;
+  }
+
+  const defaultImg =
+    "https://dummyimage.com/400x600/cdcdcd/000.jpg&text=No+poster";
 
   return (
     <div>
-      {/* <h3 className={css.detailsTitle}>{details.name}</h3>
-      <div className={css.detailsRatingBlock}>
-        <div className={css.detailsRating}>
-          <FaStar className={css.detailsStarIcon} />
-          <p className={css.detailsReviews}>
-            {details.rating} ({details.reviews ? details.reviews.length : 0}
+      <Link className={css.backLink} to={backLink.current}>
+        ← Go back
+      </Link>
+      <h3 className={css.detailsTitle}>{details.name}</h3>
+      <div className={css.ratingLocContainer}>
+        <div className={css.ratingContainer}>
+          <FaStar className={css.starIcon} />
+          <p className={css.reviews}>
+            {details.rating}({details.reviews ? details.reviews.length : 0}
             Reviews)
           </p>
         </div>
-        <div className={css.detailsRating}>
+        <div className={css.locationContainer}>
           <svg width="16" height="16">
             <use href="/sprite.svg#map" />
           </svg>
-          <p className={css.detailsLocationTitle}>{details.location}</p>
+          <p className={css.location}>{details.location}</p>
         </div>
       </div>
-      <div>
-        <p className={css.detailsPrice}>&#x20AC;{details.price}</p>
-      </div>
+      <p className={css.detailsPrice}>&#x20AC;{details.price}</p>
+      {/* <div className={css.imgContainer}> */}
       {details.gallery.length > 0 ? (
-        <ul className={css.detailsList}>
+        <ul className={css.imgContainer}>
           {details.gallery.map((item, index) => (
             <li key={index} className={css.detailsItem}>
               <img
@@ -44,20 +73,24 @@ const Details = () => {
           ))}
         </ul>
       ) : (
-        <TbPhotoCancel className={css.detailsIconImage} />
+        defaultImg
       )}
-
-      <p className={css.detailsDescription}> {details.description}</p>
-
-      <div className={css.detailsNavigations}>
-        <NavLink to="features">Features</NavLink>
-        <NavLink to="reviews">Reviews</NavLink>
-      </div>
-      <div className={css.detailsNavigationsContent}>
-        <Suspense fallback={<div>Loading ...</div>}>
-          <Outlet />
-        </Suspense>
-      </div> */}
+      {/* </div> */}
+      <p className={css.text}>{details.description}</p>
+      <nav className={css.nav}>
+        <NavLink
+          className={({ isActive }) => clsx(css.link, isActive && css.active)}
+          to="features"
+        >
+          Cast
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => clsx(css.link, isActive && css.active)}
+          to="reviews"
+        >
+          Reviwes
+        </NavLink>
+      </nav>
     </div>
   );
 };
